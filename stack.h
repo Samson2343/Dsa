032833215676bc1efa1a10d016c33ba1f3a0795e
Samson2343 <144207279+Samson2343@users.noreply.github.com>
2023-09-06 15:52:13 +0530

    updated variable names

--- stack.h
+++ stack.h
@@ -7,13 +7,13 @@ struct stack
     int top;
     int items[SIZE];
 };
-typedef struct stack st;
+typedef struct stack stack;
 
-void createStack(st *s);
-int isFull(st *s);
-int isEmpty(st *s);
-void push(st *s,int data);
-int pop(st *s);
-void printStack(st *s);
+void createStack(stack * stack);
+int isFull(stack *stack);
+int isEmpty(stack *stack);
+void push(stack *stack,int data);
+int pop(stack *stack);
+void printStack(stack *stack);
 
 #endif
