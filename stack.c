d69a16c54f49b69ef262490ba946d22312587301
Samson2343 <144207279+Samson2343@users.noreply.github.com>
2023-09-06 15:51:45 +0530

    variable names updated

--- stack.c
+++ stack.c
@@ -1,30 +1,21 @@
 #include "stack.h"
 #include<stdio.h>
-// #include<stdlib.h>
-
-// #define SIZE 10
-// struct stack
-// {
-//     int top;
-//     int items[SIZE];
-// };
-// typedef struct stack st;
 
 //create stack
-void createStack(st *s){
-    s->top = -1;
+void createStack(stack *stack){
+    stack->top = -1;
 }
 
-int isFull(st *s){
-    if(s->top==SIZE-1){
+int isFull(stack *stack){
+    if(stack->top==SIZE-1){
         return 1;
     }
     else 
     return 0;
 }
 
-int isEmpty(st *s){
-    if(s->top==-1){
+int isEmpty(stack *stack){
+    if(stack->top==-1){
         return 1;
     }
     else
@@ -32,31 +23,31 @@ int isEmpty(st *s){
 }
 
 //push
-void push(st *s,int data){
-    if(isFull(s)){
+void push(stack *stack,int data){
+    if(isFull(stack)){
         printf("STACK OVERFLOW\n");
     }
     else{
-        s->top++;
-        s->items[s->top] = data;
+        stack->top++;
+        stack->items[stack->top] = data;
     }
 }
 
-int pop(st *s){
-    if(isEmpty(s)){
+int pop(stack* stack){
+    if(isEmpty( stack)){
         printf("STACK UNDERFLOW");
     }
     else{
-    int data = s->items[s->top];
-    s->top--;
+    int data = stack->items[stack->top];
+    stack->top--;
     return data;
     }   
 }
 
-void printStack(st *s){
-    int top = s->top;
+void printStack(stack* stack){
+    int top = stack->top;
     for(int i=top;i>=0;i--){
-        printf("%d\n",s->items[i]);
+        printf("%d\n",stack->items[i]);
     }
 }
 
