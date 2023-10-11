a96637ef669485fbad9c4ba758896a3660b8c695
Rhythm Naik <rhythmstay4u@gmail.com>
2023-10-11 13:37:47 +0530

    new edit

---
+++ MergeSort/main.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>  // Include this header for string operations
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(char* arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temp arrays
+    char* L[n1];
+    char* R[n2];
+
+    // Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r]
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (strcmp(L[i], R[j]) <= 0) { // Compare strings using strcmp
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[],
+    // if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// l is for left index and r is right index of the
+// sub-array of arr to be sorted
+void mergeSort(char* arr[], int l, int r)
+{
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+// Function to print an array of strings
+void printArray(char* A[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%s ", A[i]);
+    printf("\n");
+}
+
+// Driver code
+int main()
+{
+    char* words[] = {
+        "apple", "banana", "grape", "cherry", "orange", "pear", "strawberry", "kiwi", "blueberry", "raspberry",
+        // Add more words here (up to 2000)
+    };
+    int word_count = sizeof(words) / sizeof(words[0]);
+
+    printf("Given array of words is \n");
+    printArray(words, word_count);
+
+    mergeSort(words, 0, word_count - 1);
+
+    printf("\nSorted array of words is \n");
+    printArray(words, word_count);
+    return 0;
+}
