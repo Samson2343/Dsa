df7969f6d4f0b39af29f7fe93dfc8f6a7ea5323e
Samson <samsonsurin10@gmail.com>
2023-10-13 21:43:54 +0530

    implemented shortestpath

---
+++ shortest_path/dijkstra.c
@@ -0,0 +1,44 @@
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "dijkstra.h"
+
+
+
+void dijkstra(int graph[V][V], int src)
+{
+    int dist[V]; // The output array.  dist[i] will hold the
+                 // shortest
+    // distance from src to i
+ 
+    bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
+ 
+    for (int i = 0; i < V; i++)
+        dist[i] = INT_MAX, sptSet[i] = false;
+ 
+    // Distance of source vertex from itself is always 0
+    dist[src] = 0;
+ 
+    // Find shortest path for all vertices
+    for (int count = 0; count < V - 1; count++) {
+        // Pick the minimum distance vertex from the set of vertices not yet processed.
+        int u = minDistance(dist, sptSet);
+ 
+        // Mark the picked vertex as processed
+        sptSet[u] = true;
+ 
+        // Update dist value of the adjacent vertices of the
+        // picked vertex.
+        for (int v = 0; v < V; v++)
+ 
+            // Update dist[v] only if is not in sptSet, there is an edge from u to v, and total weight of path from src to  v through u is smaller than current value of dist[v]
+            if (!sptSet[v] && graph[u][v]
+                && dist[u] != INT_MAX
+                && dist[u] + graph[u][v] < dist[v])
+                dist[v] = dist[u] + graph[u][v];
+    }
+ 
+    // print the constructed distance array
+    printSolution(dist);
+}
+
