f130b1be819bc150f5066e7bdcfdeeb5702591ec
Rhythm Naik <rhythmstay4u@gmail.com>
2023-10-14 13:13:28 +0530

    shortest path edit

--- ShortestDistance/Dijkstra/dijkstra.h/dijkstra.c
+++ ShortestDistance/Dijkstra/dijkstra.h/dijkstra.c
@@ -2,7 +2,34 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "dijkstra.h"
- 
+
+// Number of vertices in the graph
+#define V 9
+
+// A utility function to find the vertex with minimum
+// distance value, from the set of vertices not yet included
+// in shortest path tree
+int minDistance(int dist[], bool sptSet[])
+{
+	// Initialize min value
+	int min = INT_MAX, min_index;
+
+	for (int v = 0; v < V; v++)
+		if (sptSet[v] == false && dist[v] <= min)
+			min = dist[v], min_index = v;
+
+	return min_index;
+}
+
+// A utility function to print the constructed distance
+// array
+void printSolution(int dist[])
+{
+	printf("Vertex \t\t Distance from Source\n");
+	for (int i = 0; i < V; i++)
+		printf("%d \t\t\t\t %d\n", i, dist[i]);
+}
+
 // Function that implements Dijkstra's single source
 // shortest path algorithm for a graph represented using
 // adjacency matrix representation
@@ -45,4 +72,24 @@ void dijkstra(int graph[V][V], int src)
  
     // print the constructed distance array
     printSolution(dist);
+}
+ 
+// driver's code
+int main()
+{
+    /* Let us create the example graph discussed above */
+    int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+                        { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                        { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                        { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                        { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                        { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                        { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                        { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                        { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+ 
+    // Function call
+    dijkstra(graph, 0);
+ 
+    return 0;
 }
